d61771ad103bb60df2eb142bd1668840032127dc
Cocobio <iosorio@udec.cl>
2026-05-26 21:09:15 -0400

    Added simulation for max and min.

---
+++ tarea02.srcs/sim_1/new/min_max_sim.sv
@@ -0,0 +1,153 @@
+`timescale 1ns / 1ps
+///////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date: 05/25/2026 11:12:38 PM
+// Design Name:
+// Module Name: min_max_sim
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+module min_max_sim();
+    logic start, clk, rst, data_available;
+    logic [13:0] data_in;
+    logic [9:0] N;
+    logic [13:0] min, max;
+    logic done;
+
+    // Control state machine logic
+    typedef enum {
+        IDLE,
+        SETUP,
+        PRO_0, PRO_1, PRO_2,
+        DIV_1, DIV_2,
+        NEWTON_N, NEWTON_1, NEWTON_2, NEWTON_3, NEWTON_4, NEWTON_D,
+        DONE } state_e;
+
+    logic [13:0] data_in_held;
+    logic [10:0] data_counter, data_range;
+    state_e current_state, next_state;
+
+    always_comb begin
+        next_state = current_state;
+        // We can use default: next_state = next_state.next;
+        unique case (current_state)
+        IDLE: if (start) next_state = SETUP;
+        SETUP: next_state = PRO_0;
+        PRO_0: if (data_available) next_state = PRO_1;
+        PRO_1: next_state = PRO_2;
+        PRO_2: next_state = data_counter == data_range ? DIV_1 : PRO_0;
+        DIV_1: next_state = DIV_2;
+        DIV_2: next_state = NEWTON_N;
+        NEWTON_N: next_state = NEWTON_1;
+        NEWTON_1: next_state = NEWTON_2;
+        NEWTON_2: next_state = NEWTON_3;
+        NEWTON_3: next_state = NEWTON_4;
+        NEWTON_4: next_state = NEWTON_D;
+        NEWTON_D: next_state = DONE;
+        DONE: next_state = IDLE;
+        endcase
+    end
+
+    always_ff @(posedge clk) begin
+        if (rst) current_state <= IDLE;
+        else current_state <= next_state;
+    end
+
+    always_ff @(posedge clk) begin
+        if (rst) data_in_held <= '0;
+        else if (data_available) data_in_held <= data_in;
+    end
+
+
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            data_range <= '0;
+            data_counter <= '0;
+        end else if (current_state == SETUP) begin
+            // @todo check if it should be 7 or 8
+            if (N < 1016) data_range <= N + 8;
+            else data_range <= 1024;
+            data_counter <= '0;
+        end else if (current_state == PRO_1)
+            data_counter <= data_counter + 1;
+    end
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Min and Max
+    logic [13:0] minimum, maximum;
+    logic [13:0] cmp_0, cmp_1;
+    logic cmp_res;
+
+    // Tie logic to control state machine state
+    always_comb begin
+        cmp_0 = '0;
+        cmp_1 = '0;
+
+        if (current_state == PRO_1) begin
+            cmp_0 = data_in_held;
+            cmp_1 = minimum;
+        end else if (current_state == PRO_2) begin
+            cmp_0 = ~data_in_held;
+            cmp_1 = ~maximum;
+        end
+
+        cmp_res = cmp_0 < cmp_1;
+    end
+
+    always_ff @(posedge clk) begin
+        if (rst || current_state == SETUP) begin
+            minimum <= '1;
+            maximum <= '0;
+        end else if (current_state == PRO_1 && cmp_res) begin
+            minimum <= data_in_held;
+        end else if (current_state == PRO_2 && cmp_res) begin
+            maximum <= data_in_held;
+        end
+    end
+
+    // Done
+    always_comb begin
+        done = current_state == DONE;
+        if (done) begin
+            min = minimum;
+            max = maximum;
+        end else begin
+            min = '0;
+            max = '0;
+        end
+    end
+    ///////////////////////////////////////////////////////////////////////////
+    // Simulation
+    initial begin
+        clk = 0;
+        forever #1 clk = ~clk;
+    end
+
+    initial begin
+        #3 rst = 1;
+        #2 rst = 0;
+
+        #4 N = 0;
+        #1 start = 1;
+        #2 start = 0;
+        #2 N = 0;
+
+        for (int i = 0; i < 8; i = i + 1) begin
+            #16 data_available = 1; data_in = i;
+            #2 data_available = 0; data_in = 0;
+        end
+    end
+endmodule
--- tarea02.srcs/sources_1/new/processing_thread.sv
+++ tarea02.srcs/sources_1/new/processing_thread.sv
@@ -35,9 +35,16 @@ module processing_thread (
     );
 
     // Control state machine logic
-    typedef enum { IDLE, SETUP, PRO_0, PRO_1, PRO_2, DIV_1, DIV_2, DIV_3, NEWTON, DONE } state_e;
+    typedef enum {
+        IDLE,
+        SETUP,
+        PRO_0, PRO_1, PRO_2,
+        DIV_1, DIV_2,
+        NEWTON_N, NEWTON_1, NEWTON_2, NEWTON_3, NEWTON_4, NEWTON_D,
+        DONE } state_e;
 
-    logic [9:0] data_counter, data_range;
+    logic [13:0] data_in_held;
+    logic [10:0] data_counter, data_range;
     state_e current_state, next_state;
 
     always_comb begin
@@ -50,17 +57,25 @@ module processing_thread (
         PRO_1: next_state = PRO_2;
         PRO_2: next_state = data_counter == data_range ? DIV_1 : PRO_0;
         DIV_1: next_state = DIV_2;
-        DIV_2: next_state = DIV_3;
-        DIV_3: next_state = NEWTON;
-        NEWTON: next_state = DONE;
+        DIV_2: next_state = NEWTON_N;
+        NEWTON_N: next_state = NEWTON_1;
+        NEWTON_1: next_state = NEWTON_2;
+        NEWTON_2: next_state = NEWTON_3;
+        NEWTON_3: next_state = NEWTON_4;
+        NEWTON_4: next_state = newton_guess == newton_next_guess ? NEWTON_D : NEWTON_1;
+        NEWTON_D: next_state = DONE;
         DONE: next_state = IDLE;
         endcase
     end
 
+    always_ff @(posedge clk) begin
+        if (rst) current_state <= IDLE;
+        else current_state <= next_state;
+    end
 
     always_ff @(posedge clk) begin
-        if (rst) current_state = IDLE;
-        else current_state = next_state;
+        if (rst) data_in_held <= '0;
+        else if (data_available) data_in_held <= data_in;
     end
 
     always_ff @(posedge clk) begin
@@ -69,8 +84,8 @@ module processing_thread (
             data_counter <= '0;
         end else if (current_state == SETUP) begin
             // @todo check if it should be 7 or 8
-            if (N < 8) data_range <= 7; // at least 8 data
-            else data_range <= N;
+            if (N < 1016) data_range <= N + 8;
+            else data_range <= 1024;
             data_counter <= '0;
         end else if (current_state == PRO_1)
             data_counter <= data_counter + 1;
@@ -88,10 +103,10 @@ module processing_thread (
         cmp_1 = '0;
 
         if (current_state == PRO_1) begin
-            cmp_0 = data_in;
+            cmp_0 = data_in_held;
             cmp_1 = minimum;
         end else if (current_state == PRO_2) begin
-            cmp_0 = ~data_in;
+            cmp_0 = ~data_in_held;
             cmp_1 = ~maximum;
         end
 
@@ -102,23 +117,24 @@ module processing_thread (
         if (rst || current_state == SETUP) begin
             minimum <= '1;
             maximum <= '0;
-        end else if (current state == PRO_1 && cmp_res) begin
-            minimum <= data_in;
-        end else if (current state == PRO_2 && cmp_res) begin
-            maximum <= data_in;
+        end else if (current_state == PRO_1 && cmp_res) begin
+            minimum <= data_in_held;
+        end else if (current_state == PRO_2 && cmp_res) begin
+            maximum <= data_in_held;
         end
     end
     ///////////////////////////////////////////////////////////////////////////
-    
+
     // Mean and RMS
     logic [23:0] mean;
     logic [25:0] rms;
 
-    logic [27:0] mul_0;
-    logic [17:0] mul_1;
-    logic [45:0] mul_res;
+    // DSP48E1 25x18 signed multiplication
+    logic [23:0] mul_0;
+    logic [16:0] mul_1;
+    logic [40:0] mul_res;
 
-    logic [25:0] sum_0;
+    logic [24:0] sum_0;
     logic [17:0] sum_1;
     logic [25:0] sum_res;
 
@@ -127,12 +143,104 @@ module processing_thread (
         if (rst || current_state == SETUP) begin
             mean <= '0;
             rms <= '0;
-        end else if (current_state == PRO_1)
+        end else if (current_state == PRO_1) begin
             mean <= sum_res[23:0];
-        else if (current_state == PRO_2)
+        end else if (current_state == PRO_2) begin
             rms <= sum_res;
+        end else if (current_state == DIV_1) begin
+            mean <= mul_res[25:10];     // @todo check the slices
+        end else if (current_state == DIV_2) begin
+            rms <= mul_res[27:10];      // @todo check the slices
+        end
     end
+
     ///////////////////////////////////////////////////////////////////////////
 
     // Division
+    // Initialize LUT
+    localparam DENOM_WIDTH = 12;
+    logic [DENOM_WIDTH-1:0] mem[1024];
+    initial $readmemh("denom_division.mem", mem);
+    logic [DENOM_WIDTH-1:0] denominator;
+
+    always_ff @(posedge clk) begin
+        if (rst) denominator <= '0;
+        else if (current_state == SETUP) denominator <= mem[data_range];
+    end
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Newton
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Sum
+    always_comb begin
+        case (current_state)
+        PRO_1: begin
+            sum_0 = {'0, mean};
+            sum_1 = {'0, data_in_held};
+        end PRO_2: begin
+            sum_0 = rms;
+            sum_1 = {'0, data_in_held};
+        end NEWTON_4: begin
+            sum_0 = {'0, ~newton_next_guess + 1};
+            sum_1 = 2'd3;
+        end default: begin
+            sum_0 = '0;
+            sum_1 = '0;
+        end endcase
+
+        sum_res = sum_0 + sum_1;
+    end
+
+    // Mult
+    logic [23:0] mul_0;
+    logic [16:0] mul_1;
+    logic [40:0] mul_res;
+    always_comb begin
+        case (current_state)
+        PRO_1: begin
+            mul_0 = {'0, data_in_held};
+            mul_1 = {'0, data_in_held};
+        end DIV_1: begin
+            mul_0 = {'0, mean};
+            mul_1 = {'0, denominator};
+        end DIV_2: begin
+            mul_0 = rms[25:2];
+            mul_1 = {'0, denominator};
+        end NEWTON_1: begin
+            mul_0 = {'0, newton_guess};
+            mul_1 = {'0, newton_guess};
+        end NEWTON_2: begin
+            mul_0 = {'0, newton_guess};
+            mul_1 = {'0, newton_target};
+        end NEWTON_4: begin
+            mul_0 = {'0, newton_guess};
+            mul_1 = {'0, newton_next_guess};
+        end default: begin
+            mul_0 = '0;
+            mul_1 = '0;
+        end endcase
+
+        mul_res = mul_0 + mul_1;
+    end
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Done
+    always_comb begin
+        done = current_state == DONE;
+        if (done) begin
+            min = minimum;
+            max = maximum;
+            mean = mean;
+            rms = rms;
+        end else begin
+            min = '0;
+            max = '0;
+            mean = '0;
+            rms = '0;
+        end
+    end
+    ///////////////////////////////////////////////////////////////////////////
 endmodule
