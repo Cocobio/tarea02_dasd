c2124e5154d76dd703603f87fdca8021113f178c
Cocobio <iosorio@udec.cl>
2026-05-30 18:11:19 -0400

    Add second iteration on the visualization_thread.

--- tarea02.srcs/sources_1/new/visualization_thread.sv
+++ tarea02.srcs/sources_1/new/visualization_thread.sv
@@ -20,55 +20,94 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
-module visualization_thread( input logic clk,
-                                         rst,
-                                         done, //señal termino calculo de métricas
+module visualization_thread(input logic clk,
+                                        rst,
+                                        done, //señal termino calculo de métricas
                             input logic [13:0] min,
                                                max,
                                                mean,
                                                rms,
-                                               sw,
+                            input logic [1:0] sw,
                             output logic [11:0] result //CONVERSION: multiplicacion *1000
                             );
 
-    //DEFINICION DE ESTADOS
-    typedef enum logic{SELECT, //PARA VER EN QUE COMB DE SW COMIENZA
-                       CONVERT} //SEPARAR VALORES PARA DISPLAY
-     state_t ;
-     state_t state , next_state ;
+    typedef enum {DISPLAY,
+                  READ,
+                  NORMALIZE_1,
+                  NORMALIZE_2,
+                  NORMALIZE_3} state_e;
+    state_e current_state, next_state;
 
-    logic [13:0] valor_ing; //valor_ingresado a convertir
-
-    //TRANSICION DE ESTADOS
-    always_ff@(posedge clk)begin
-    if(done == 1)
-        state <= next_state;
-    end
-
-    //ELEGIR SIGUIENTE ESTADO
     always_comb begin
-        case(state)
-            SELECT: next_state = CONVERT;
-            CONVERT: next_state = SELECT;
+        next_state = current_state;
+        case (current_state)
+        DISPLAY: if (done) next_state = NORMALIZE_0;
+        NORMALIZE_0: next_state = NORMALIZE_1;
+        NORMALIZE_1: next_state = NORMALIZE_2;
+        NORMALIZE_2: next_state = NORMALIZE_3;
+        NORMALIZE_3: next_state = DISPLAY;
         endcase
     end
 
+    always_ff @(posedge clk) begin
+        if (rst) current_state <= DISPLAY;
+        else current_state <= next_state;
+    end
 
-    //QUE HACER EN CADA ESTADO
-    always_ff@(posedge clk)begin
-        case(state)
-        SELECT: begin
-            case(sw) //QUE VALOR INGRESAR CON CADA SWITCH
-                2'b00: valor_ing <= min;
-                2'b01: valor_ing <= max;
-                2'b10: valor_ing <= mean;
-                2'b11: valor_ing <= rms;
-            endcase
-        end
+    localparam min_init = 0;
+    localparam max_init = 1;
+    localparam mean_init = 2;
+    localparam rms_init = 3;
+    logic [13:0] internal_min = min_init,
+                 internal_max = max_init,
+                 internal_mean = mean_init,
+                 internal_rms = rms_init;
 
-        CONVERT: begin //MULTIPLICAR POR 1000 PARA CONVERTIR
-            result <= (valor_ing * 1000) >> 12;
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            internal_min <= min_init;
+            internal_max <= max_init;
+            internal_mean <= mean_init;
+            internal_rms <= rms_init;
+        end else if (current_state == DISPLAY && done) begin
+            internal_min <= min;
+            internal_max <= max;
+            internal_mean <= mean;
+            internal_rms <= rms;
+        end else if (current_state == NORMALIZE_0) begin
+            internal_min <= mul_res >> 12;
+        end else if (current_state == NORMALIZE_1) begin
+            internal_max <= mul_res >> 12;
+        end else if (current_state == NORMALIZE_2) begin
+            internal_mean <= mul_res >> 12;
+        end else if (current_state == NORMALIZE_3) begin
+            internal_rms <= mul_res >> 12;
         end
+    end
+
+    logic [13:0] mul_0;
+    logic [25:0] mul_res;
+
+    always_comb begin
+        case (current_state)
+        NORMALIZE_0: mul_0 = internal_min;
+        NORMALIZE_1: mul_0 = internal_max;
+        NORMALIZE_2: mul_0 = internal_mean;
+        NORMALIZE_3: mul_0 = internal_rms;
+        default: mul_0 = '0;
         endcase
+
+        mul_res = mul_0 * 10'd1000;
+    end
+
+    always_comb begin
+        if (current_state == DISPLAY) begin
+            case (sw)
+            2'b00: result = internal_min;
+            2'b01: result = internal_max;
+            2'b10: result = internal_mean;
+            2'b11: result = internal_rms;
+            endcase
+        end else result = 0;
     end
 endmodule : visualization_thread
